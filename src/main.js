3afb887b35a47c07ebda8c457c417ee3f512fe1d
qkenn <github.com.sampling990@passinbox.com>
2024-08-03 21:52:26 +0530

    feat(component): add dropdown component, basic styling

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('hello');
+import './style.css';
+import chevronIcon from './icons/chevron-down.svg';
+
+const dropIcon = document.getElementById('chevron-icon'),
+  dropLink = document.getElementById('drop-link');
+
+dropIcon.setAttribute('src', chevronIcon);
+
+dropLink.addEventListener('mouseenter', function () {
+  console.log('hovering');
+});
