46e23bf5443ce753e81d39ed56765f9197df4472
qkenn <github.com.sampling990@passinbox.com>
2024-08-04 10:54:19 +0530

    feat(ui): add content for hidden menu

--- webpack.config.js
+++ webpack.config.js
@@ -27,16 +27,16 @@ const commonConfig = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      // {
-      //   test: /\.(?:js|mjs|cjs)$/,
-      //   exclude: /node_modules/,
-      //   use: {
-      //     loader: 'babel-loader',
-      //     options: {
-      //       presets: [['@babel/preset-env', { targets: 'defaults' }]],
-      //     },
-      //   },
-      // },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
