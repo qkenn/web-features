9d58a8281b02868f6327f5049af6c99d6154e5a2
qkenn <github.com.sampling990@passinbox.com>
2024-08-04 11:40:50 +0530

    refactor(component): implement dropdown menu feature with classes

--- src/main.js
+++ src/main.js
@@ -1,39 +1,63 @@
 import './style.css';
 import chevronIcon from './images/chevron-down.svg';
 
+// set dropdown menu icon
 document.getElementById('chevron-icon').setAttribute('src', chevronIcon);
 
-const dropLink = document.getElementById('drop-link'),
-  hiddenMenu = document.getElementById('hidden-menu');
+// dropdown class
+class Dropdown {
+  constructor(trigger, hiddenMenu) {
+    this.trigger = trigger;
+    this.hiddenMenu = hiddenMenu;
 
-dropLink.addEventListener('mouseenter', () => {
-  hiddenMenu.style.display = 'block';
+    // listen for events soon after dropdown gets created
+    this.reveal();
+    this.hide();
+  }
 
-  const fadeIn = hiddenMenu.animate(
-    [
-      { opacity: 0, transform: 'translateY(0)' },
-      { opacity: 1, transform: 'translateY(.5rem)' },
-    ],
-    {
-      duration: 200,
-      fill: 'forwards',
-    }
-  );
-});
-
-dropLink.addEventListener('mouseleave', () => {
-  const fadeOut = hiddenMenu.animate(
-    [
-      { opacity: 1, transform: 'translateY(.5rem)' },
-      { opacity: 0, transform: 'translateY(0)' },
-    ],
-    {
-      duration: 200,
-      fill: 'forwards',
-    }
-  );
-
-  fadeOut.onfinish = () => {
-    hiddenMenu.style.display = 'none';
+  // hidden menu reveal event
+  reveal = () => {
+    this.trigger.addEventListener('mouseenter', this.handleReveal);
   };
-});
+
+  // reveal event handler
+  handleReveal = () => {
+    // remove initial hidden state
+    this.hiddenMenu.style.display = 'block';
+
+    // reveal with a slight effect
+    this.hiddenMenu.animate(
+      [{ opacity: 0 }, { opacity: 1, transform: 'translateY(.5rem)' }],
+      {
+        duration: 200,
+        fill: 'forwards',
+      }
+    );
+  };
+
+  // hidden menu hide event
+  hide = () => {
+    this.trigger.addEventListener('mouseleave', this.handleHide);
+  };
+
+  // hide event handler
+  handleHide = () => {
+    // hide with a slight effect
+    this.hiddenMenu.animate(
+      [{ opacity: 1 }, { opacity: 0, transform: 'translateY(0)' }],
+      {
+        duration: 200,
+        fill: 'forwards',
+      }
+
+      // hide after effect displayed
+    ).onfinish = () => {
+      this.hiddenMenu.style.display = 'none';
+    };
+  };
+}
+
+const dropdown = new Dropdown(
+  document.getElementById('drop-link'),
+  document.getElementById('hidden-menu')
+);
