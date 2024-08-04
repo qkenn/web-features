399808e6697e11665b7d4886f2a6bb562dfb73ca
qkenn <github.com.sampling990@passinbox.com>
2024-08-04 10:28:07 +0530

    feat(compnent) reveal hidden menu on hover

--- dist/app.bundle.js
+++
@@ -1,2 +0,0 @@
-console.log("hello");
-//# sourceMappingURL=app.bundle.js.map
--- src/main.js
+++ src/main.js
@@ -2,10 +2,26 @@ import './style.css';
 import chevronIcon from './icons/chevron-down.svg';
 
 const dropIcon = document.getElementById('chevron-icon'),
-  dropLink = document.getElementById('drop-link');
+  dropLink = document.getElementById('drop-link'),
+  hiddenMenu = document.getElementById('hidden-menu');
 
 dropIcon.setAttribute('src', chevronIcon);
 
-dropLink.addEventListener('mouseenter', function () {
-  console.log('hovering');
+dropLink.addEventListener('mouseenter', () => {
+  hiddenMenu.style.display = 'block';
+
+  const fadeIn = hiddenMenu.animate([{ opacity: 0 }, { opacity: 1 }], {
+    duration: 500,
+    fill: 'forwards',
+  });
+});
+
+dropLink.addEventListener('mouseleave', () => {
+  const fadeOut = hiddenMenu.animate([{ opacity: 1 }, { opacity: 0 }], {
+    duration: 500,
+  });
+
+  fadeOut.onfinish = () => {
+    hiddenMenu.style.display = 'none';
+  };
 });
